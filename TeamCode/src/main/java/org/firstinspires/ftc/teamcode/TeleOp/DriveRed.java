7ab8b2041e7d463eb5960328445d3b34ff3b33d5
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-10-20 14:53:57 -0700

    added move to set pos implementation, modeled heavily on the TylerAuto way(cerror)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveRed.java
@@ -5,12 +5,34 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Auto.TylerAuto;
 import org.firstinspires.ftc.teamcode.HardwareSoftware;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @TeleOp(name="DriveRed")
 public class DriveRed extends OpMode {
     HardwareSoftware hw = new HardwareSoftware();
+
+    double[] initPositions = {0,0,0};
+
+    DriveRed.CurrentRobotPose currentPose = new CurrentRobotPose();
+
+    SparkFunOTOS.Pose2D pos;
+
+    double cTreshold = .5;
+
+    boolean isMovingToSetPos = false;
+
+    boolean isOkToMoveOn = false;
+
+
 
     @Override
     public void init() {
@@ -19,6 +41,22 @@ public class DriveRed extends OpMode {
 
         hw.gyro().calibrateImu();
         hw.gyro().resetTracking();
+
+        hw.gyro.setLinearUnit(DistanceUnit.INCH);
+        hw.gyro.setAngularUnit(AngleUnit.RADIANS);
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        hw.gyro.setOffset(offset);
+        hw.gyro.setLinearScalar(1.0);
+        hw.gyro.setAngularScalar(1.0);
+        hw.gyro.calibrateImu();
+        hw.gyro.resetTracking();
+        SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
+        hw.gyro.setPosition(currentPosition);
+
+        //this is where you add all of the locations for the robot to go to
+
+
+        currentPose.init(hw,initPositions[0],initPositions[1],initPositions[2]);
 
     }
 
@@ -45,10 +83,18 @@ public class DriveRed extends OpMode {
 
 
         double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-        hw.FLdrive().setPower(((rotY + rotX + rx) / denominator) * DS);
-        hw.BLdrive().setPower(((rotY - rotX + rx) / denominator) * DS);
-        hw.FRdrive().setPower(((rotY - rotX - rx) / denominator) * DS);
-        hw.BRdrive().setPower(((rotY + rotX - rx) / denominator) * DS);
+
+        if(!isMovingToSetPos){
+            hw.FLdrive().setPower(((rotY + rotX + rx) / denominator) * DS);
+            hw.BLdrive().setPower(((rotY - rotX + rx) / denominator) * DS);
+            hw.FRdrive().setPower(((rotY - rotX - rx) / denominator) * DS);
+            hw.BRdrive().setPower(((rotY + rotX - rx) / denominator) * DS);
+        }
+
+
+        if(gamepad1.a){
+            driveToShootingPosFar(new NewPositionOfRobot(5,6,4));
+        }
 
 //        if (gamepad1.b) {
 //            hw.Lucket().setPosition(.02);
@@ -102,5 +148,206 @@ public class DriveRed extends OpMode {
         if (gamepad1.share){
             hw.gyro().resetTracking();
         }
+
+
+
+    }
+
+    public void driveToShootingPosFar(NewPositionOfRobot setPos){
+        isMovingToSetPos = true;
+
+        double cerror;
+
+        while (!isOkToMoveOn) {
+            pos = hw.gyro.getPosition();
+            telemetry.addData("Posx", pos.x);
+            telemetry.addData("Posy", pos.y);
+            telemetry.addData("Posh", pos.h);
+            telemetry.update();
+
+            currentPose.gyX = pos.x;
+            currentPose.gyY = pos.y;
+            currentPose.gyR = pos.h;
+
+            currentPose.updateRealRobotPositions(pos);
+
+            cerror = currentPose.moveToSetPosition(setPos);
+
+            telemetry.addData("cerror", cerror);
+
+
+            if (Math.abs(cerror) < cTreshold) {
+                isOkToMoveOn = true;
+            }
+
+        }
+
+        isMovingToSetPos = false;
+        isOkToMoveOn = false;
+    }
+
+
+    /**
+     * Represents the new position and rotation of the robot.
+     */
+    public class NewPositionOfRobot {
+        boolean justDrive;
+        double newx, newy;
+        double newRotation;
+        double speed = .8;
+        /**
+         * Sets the robot's future position and rotation.
+         *
+         * @param nx     The new x-coordinate.
+         * @param ny     The new y-coordinate.
+         * @param newRot The new rotation angle.
+         */
+        NewPositionOfRobot(double nx, double ny, double newRot) {
+            this.newx = nx;
+            this.newy = ny;
+            this.newRotation = newRot;
+            justDrive = true;
+        }
+        NewPositionOfRobot(double nx, double ny, double newRot, double setspeed) {
+            this.newx = nx;
+            this.newy = ny;
+            this.newRotation = newRot;
+            this.speed = setspeed;
+            justDrive = true;
+        }
+
+    }
+
+    public class CurrentRobotPose {
+        HardwareSoftware robotHardwaremap;
+        double realRobotX, realRobotY, realRobotHeading;
+        double gyX, gyY, gyR;
+
+        double initX, initY, initZ;
+
+        /**
+         * Initializes the robot's pose.
+         *
+         * @param hwMap The hardware map.
+         * @param inX   The initial x-coordinate.
+         * @param inY   The initial y-coordinate.
+         * @param inz   The initial z-coordinate.
+         */
+        public void init(HardwareSoftware hwMap, double inX, double inY, double inz) {
+            this.robotHardwaremap = hwMap;
+            this.initX = inX;
+            this.initY = inY;
+            this.initZ = inz;
+        }
+
+        /**
+         * Updates the robot's real positions based on gyroscope values.
+         *
+         * @param gyroValue The current gyroscope position.
+         */
+        public void updateRealRobotPositions(SparkFunOTOS.Pose2D gyroValue) {
+            gyX = gyroValue.x;
+            gyY = gyroValue.y;
+            gyR = gyroValue.h;
+
+            realRobotX = initX + gyX;
+            realRobotY = initY + gyY;
+            realRobotHeading = initZ + normalizeAngle(gyR);
+        }
+
+        /**
+         * Moves the robot to a set position and returns the current error.
+         *
+         * @param setPose The target position and rotation.
+         * @return The current error between the robot's position and the target position.
+         */
+        double moveToSetPosition(DriveRed.NewPositionOfRobot setPose) {
+            double currentError = 0;
+            double powY, powX, rx = 0;
+            double powdY, powdX;
+
+            powdX = setPose.newx - realRobotX;
+            powdY = setPose.newy - realRobotY;
+
+            if (powdX > 3 || powdX < -3) {
+                powX = Math.signum(powdX);
+            } else {
+                powX = powdX/3;
+            }
+
+            if (powdY > 3 || powdY < -3) {
+                powY = Math.signum(powdY);
+            } else {
+                powY = powdY/3;
+            }
+
+            double[] altAngles = new double[3];
+            double[] diffAngles = new double[3];
+
+            altAngles[0] =  setPose.newRotation - 2*Math.PI;
+            altAngles[1] =  setPose.newRotation            ;
+            altAngles[2] =  setPose.newRotation + 2*Math.PI;
+
+            for (int i = 0; i < 3; ++i) {
+                diffAngles[i] = altAngles[i] - realRobotHeading;
+            }
+
+            Arrays.sort(diffAngles);
+
+            int goodindex = 0;
+
+            if (Math.abs(diffAngles[1]) < Math.abs(diffAngles[0])) {
+                goodindex =1;
+            }
+            if (Math.abs(diffAngles[2]) < Math.abs(diffAngles[0])) {
+                goodindex = 2;
+            }
+            if (Math.abs(diffAngles[2]) < Math.abs(diffAngles[1])) {
+                goodindex = 2;
+            }
+            if (Math.abs(diffAngles[1]) < Math.abs(diffAngles[2])) {
+                goodindex = 1;
+            }
+
+            rx = diffAngles[goodindex];
+
+            telemetry.addData("diffIn0", diffAngles[0]);
+            telemetry.addData("diffIn1", diffAngles[1]);
+            telemetry.addData("diffIn2", diffAngles[2]);
+
+
+            double realSetY = powY * Math.cos(realRobotHeading) - powX * Math.sin(realRobotHeading);
+            double realSetX = powY * Math.sin(realRobotHeading) + powX * Math.cos(realRobotHeading);
+
+            telemetry.addData("powx", powX);
+            telemetry.addData("powy", powY);
+            telemetry.addData("realSetX", realSetX);
+            telemetry.addData("realSetY", realSetY);
+            telemetry.addData("rx", rx);
+
+
+
+
+            double denominator = Math.max(Math.abs(powY) + Math.abs(powX) + Math.abs(rx), 1);
+
+            hw.FLdrive.setPower((( -realSetY - realSetX - rx) / denominator) * setPose.speed);
+            hw.BLdrive.setPower((( -realSetY + realSetX - rx) / denominator) * setPose.speed);
+            hw.FRdrive.setPower((( -realSetY + realSetX + rx) / denominator) * setPose.speed);
+            hw.BRdrive.setPower((( -realSetY - realSetX + rx) / denominator) * setPose.speed);
+            currentError = Math.abs(powdX) + Math.abs(powdY) + Math.abs(rx);
+
+            return currentError;
+        }
+
+    }
+
+    public static double normalizeAngle(double angle) {
+        while (angle < 0) {
+            angle += 2 * Math.PI;
+        }
+        while (angle >= 2 * Math.PI) {
+            angle -= 2 * Math.PI;
+        }
+        return angle;
     }
 }
