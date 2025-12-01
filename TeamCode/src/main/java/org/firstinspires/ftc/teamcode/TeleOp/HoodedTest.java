5553d81a6d7b6110bfcbf4dc4540c339dcb9eb5f
Knights8081 <Knights8081@gcsaz.org>
2025-12-01 15:38:06 -0700

    added class to test hooded shooter prototype

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/HoodedTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.HardwareSoftware;
+@TeleOp(name = "Hood Test")
+public class HoodedTest extends OpMode {
+    HardwareSoftware robot = new HardwareSoftware();
+    double pos = 0.5;
+
+    @Override
+    public void init() {
+        robot.init(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.right_trigger > 0.5){
+            robot.flyWheel.setPower(0.7);
+        } else {
+            robot.flyWheel.setPower(0);
+        }
+
+        if(gamepad1.a){
+            pos += 0.02;
+        }
+        if(gamepad1.b){
+            pos -= 0.02;
+        }
+        robot.flyWheelRotator2.setPosition(pos);
+
+
+    }
+}
