8f916b30444b327b0e3babe591cf12e06923957e
8081Knights <89168074+8081Knights@users.noreply.github.com>
2025-12-01 13:47:20 -0700

    Refactor ColorSensor to use normalized color readings
    Updated color sensor readings to use normalized colors and added conditions for detecting green and purple.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ColorSensor.java
@@ -2,28 +2,47 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
 @TeleOp(name="colorSensorTest")
 public class ColorSensor extends OpMode {
 
     RevColorSensorV3 colorSensor;
+    NormalizedRGBA colors;
+
+    float r;
+    float g;
+    float b;
+
     int red;
     int green;
     int blue;
 
     public void init() {
         colorSensor = hardwareMap.get(RevColorSensorV3.class, "ColorSensor");
+        colorSensor.setGain(2);
     }
 
     @Override
     public void loop() {
-        red = colorSensor.red();
-        green = colorSensor.green();
-        blue = colorSensor.blue();
+        colors = colorSensor.getNormalizedColors();
 
-        telemetry.addData("red", red);
+         r = colors.red;
+         g = colors.green;
+         b = colors.blue;
+
+         red = (int)(r * 255);
+        green = (int)(g * 255);
+        blue = (int)(b * 255);
+        if (green >= 25 && blue <= 30 && red <= 10){
+            telemetry.addData("Is Green", true);
+        }
+        if (blue >= 25 && red >= 10 && green <= 15) {
+            telemetry.addData("Is Purple", true);
+        }
         telemetry.addData("green", green);
         telemetry.addData("blue", blue);
         telemetry.update();
     }
 }
+
