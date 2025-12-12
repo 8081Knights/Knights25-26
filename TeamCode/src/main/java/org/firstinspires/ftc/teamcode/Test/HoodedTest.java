a7b1f0b9ee42a288ed862bac6284dceeec041e54
TJPhoenix64 <tyler.jordan@gcsaz.org>
2025-12-12 14:04:10 -0700

    added more functionality to test hooded shooter and turntable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/HoodedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/HoodedTest.java
@@ -1,46 +1,56 @@
 package org.firstinspires.ftc.teamcode.Test;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.HardwareSoftware;
-@Disabled
+
 @TeleOp(name = "Hood Test")
 public class HoodedTest extends OpMode {
-    HardwareSoftware robot = new HardwareSoftware();
-    double pos = 0.5;
+	private DcMotorEx flyWheel = null;
 
-    @Override
-    public void init() {
-        robot.init(hardwareMap);
+	private Servo flyWheelRotator = null;
+	private DcMotorEx turnTableRotator = null;
+	double pos = 0.5;
 
-    }
+	@Override
+	public void init() {
+		flyWheelRotator = hardwareMap.get(Servo.class, "flyWheelRotator");
+		turnTableRotator = hardwareMap.get(DcMotorEx.class, "turnTableRotator");
+		flyWheel = hardwareMap.get(DcMotorEx.class, "flyWheel");
 
-    @Override
-    public void loop() {
+	}
 
-        if(gamepad1.x){
-            robot.intake.setPower(0.5);
-            telemetry.addData("HI treyton", "JO momma");
-        } else {
-            robot.intake.setPower(0);
-        }
+	@Override
+	public void loop() {
 
-        if(gamepad1.right_trigger > 0.5){
-            robot.flyWheel.setPower(0.7);
-        } else {
-            robot.flyWheel.setPower(0);
-        }
+		if (gamepad1.x) {
+			turnTableRotator.setPower(0.3);
+		} else if (gamepad1.y) {
+			turnTableRotator.setPower(-0.3);
+		} else {
+			turnTableRotator.setPower(0);
+		}
 
-        if(gamepad1.a){
-            pos += 0.02;
-        }
-        if(gamepad1.b){
-            pos -= 0.02;
-        }
-        robot.flyWheelRotator2.setPosition(pos);
+		if (gamepad1.right_trigger > 0.5) {
+			flyWheel.setPower(-0.9);
+			telemetry.addData("Flywheel velo", flyWheel.getVelocity());
+			telemetry.addData("flywheel power", flyWheel.getPower());
+		} else {
+			flyWheel.setPower(0);
+		}
+
+		if (gamepad1.a) {
+			pos += 0.02;
+		}
+		if (gamepad1.b) {
+			pos -= 0.02;
+		}
+		flyWheelRotator.setPosition(pos);
 
 
-    }
+	}
 }
