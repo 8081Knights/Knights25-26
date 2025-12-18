de52b4f213414c886a396ec494122820534b29f2
Knights8081 <Knights8081@gcsaz.org>
2025-12-18 09:11:31 -0700

    chore: removed unused import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSoftware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareSoftware.java
@@ -6,7 +6,6 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
