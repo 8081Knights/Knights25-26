7ab69e2235d76fcfad808c67dd9fcbd047551974
Knights8081 <Knights8081@gcsaz.org>
2026-01-27 14:26:50 -0700

    started making sorter subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Sorter.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import org.firstinspires.ftc.teamcode.HardwareSoftware;
+
+public class Sorter {
+    public HardwareSoftware hw;
+    public void setHardwareMap(HardwareSoftware hw){
+        this.hw = hw;
+    }
+
+
+}
