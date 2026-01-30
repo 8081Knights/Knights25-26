c28cf8b01ec5e5aad656fcd98fd7818532011e32
TJPhoenix64 <tyler.jordan@gcsaz.org>
2026-01-30 13:52:55 -0700

    added drive ability to turn cam test and moved to servo for turntable instead of motor, refined logic more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TurnCamTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TurnCamTest.java
@@ -4,9 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-
 
 
 import org.firstinspires.ftc.vision.VisionPortal;
@@ -14,75 +14,101 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
+
 @TeleOp(name = "turnCamTest")
 public class TurnCamTest extends OpMode {
-    DcMotorEx turntable;
+	Servo turntable;
 
-    VisionPortal visionPortal;
+	VisionPortal visionPortal;
 
-    AprilTagProcessor aprilTag;
+	AprilTagProcessor aprilTag;
 
-    List<AprilTagDetection> detections;
+	List<AprilTagDetection> detections;
 
-    AprilTagDetection bestDet = null;
-    double bearing = 0;
+	AprilTagDetection bestDet = null;
+	double bearing = 0;
 
-    double ticksPerDegree = 4.71472;
+	double ticksPerDegree = 4.71472;
+
+	double pastServoPos;
+
+	double targetServoPos;
+
+	public DcMotorEx FRdrive = null;
+	public DcMotorEx BRdrive = null;
+	public DcMotorEx BLdrive = null;
+	public DcMotorEx FLdrive = null;
 
 
-    @Override
-    public void init() {
-        turntable = hardwareMap.get(DcMotorEx.class, "turntable");
-        turntable.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+	@Override
+	public void init() {
+
+		FLdrive = hardwareMap.get(DcMotorEx.class, "FLdrive");
+		FRdrive = hardwareMap.get(DcMotorEx.class, "FRdrive");
+		BLdrive = hardwareMap.get(DcMotorEx.class, "BLdrive");
+		BRdrive = hardwareMap.get(DcMotorEx.class, "BRdrive");
+		BLdrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+		turntable = hardwareMap.get(Servo.class, "turntable");
+		turntable.setPosition(0.5);
+		pastServoPos = turntable.getPosition();
+		targetServoPos = pastServoPos;
 
 
+		aprilTag = new AprilTagProcessor.Builder()
+				.setDrawAxes(true)
+				.setDrawCubeProjection(true)
+				.setDrawTagOutline(true)
+				.build();
 
-        aprilTag = new AprilTagProcessor.Builder()
-                .setDrawAxes(true)
-                .setDrawCubeProjection(true)
-                .setDrawTagOutline(true)
-                .build();
+		visionPortal = new VisionPortal.Builder()
+				.setCamera(hardwareMap.get(org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName.class, "Webcam 1"))
+				.addProcessor(aprilTag)
+				.build();
 
-        visionPortal = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName.class, "Webcam 1"))
-                .addProcessor(aprilTag)
-                .build();
+	}
 
-    }
+	@Override
+	public void loop() {
 
-    @Override
-    public void loop() {
+		detections = aprilTag.getDetections();
+		if (detections.isEmpty()) {
+			bestDet = null;
+		} else {
+			for (AprilTagDetection det : detections) {
+				//if (det.id == 24 || det.id == 20) {
+				bestDet = det;
+				//}
+			}
+		}
 
-        detections = aprilTag.getDetections();
-        if(detections.isEmpty()){
-            bestDet = null;
-        } else {
-            for (AprilTagDetection det : detections) {
-                //if (det.id == 24 || det.id == 20) {
-                bestDet = det;
-                //}
-            }
-        }
+		if (bestDet != null) {
+			bearing = bestDet.ftcPose.bearing;
+		} else {
+			bearing = 0;
+		}
 
-        if(bestDet != null){
-            bearing = bestDet.ftcPose.bearing;
-        } else {
-            bearing = 0;
-        }
+		telemetry.addData("bearing", bearing);
+		telemetry.addData("tag", bestDet);
 
-        telemetry.addData("bearing", bearing);
-        telemetry.addData("tag", bestDet);
+		double kP = 0.006; // tune
 
-        double kP = 0.006; // tune
+		double turnPower = kP * bearing;
 
-        double turnPower = kP * bearing;
-        turnPower = Range.clip(turnPower, -0.3, 0.3);
-        if(gamepad1.a){
-            turntable.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            turntable.setPower(turnPower);
-        } else {
-            turntable.setPower(0);
-        }
+		turnPower = Range.clip(turnPower, -0.3, 0.3);
+		if (gamepad1.a) {
+			double hi = Range.clip((targetServoPos + turnPower), 0, 1);
+			turntable.setPosition(hi);
+			telemetry.addData("Avelo", hi);
+			pastServoPos = targetServoPos;
+		} else if (gamepad1.b) {
+			turntable.setPosition(Range.clip((turntable.getPosition() + 0.00003), 0, 1));
+		} else if (gamepad1.y) {
+			turntable.setPosition(Range.clip((turntable.getPosition() - 0.0003), 0, 1));
+		}
+
+
+		manualMechanumDrive();
 
 
 //        if(gamepad1.a){
@@ -107,15 +133,17 @@ public class TurnCamTest extends OpMode {
 //            turntable.setVelocity(0);
 //        }
 
-        telemetry.addData("velocity", turntable.getVelocity());
-        telemetry.addData("pos", turntable.getCurrentPosition());
+		telemetry.addData("velocity", turnPower);
+		telemetry.addData("pos", turntable.getPosition());
+		telemetry.addData("pastPos", pastServoPos);
+		telemetry.addData("targetPos", targetServoPos);
 
+		telemetry.update();
+	}
 
-    }
-
-    public int degreesToTicks(int degrees){
-        return (int)(degrees * ticksPerDegree);
-    }
+	public int degreesToTicks(int degrees) {
+		return (int) (degrees * ticksPerDegree);
+	}
     /*
     public void turnToTag(){
         while (bearing > 100) {
@@ -142,4 +170,17 @@ public class TurnCamTest extends OpMode {
     }
 
      */
+
+
+	public void manualMechanumDrive() {
+		double y = -gamepad1.left_stick_y;
+		double x = gamepad1.left_stick_x;
+		double rx = -gamepad1.right_stick_x;
+
+		//headless
+		FLdrive.setPower(y + rx + x);
+		FRdrive.setPower(y - rx - x);
+		BLdrive.setPower(y + rx - x);
+		BRdrive.setPower(y - rx + x);
+	}
 }
