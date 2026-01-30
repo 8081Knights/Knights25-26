191ec51f75647c7a1308b3f6cc69d327f6bdd647
TJPhoenix64 <tyler.jordan@gcsaz.org>
2026-01-30 13:50:28 -0700

    undisabled and moved from hardware software to in class hardware map

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/DriveTest.java
@@ -3,82 +3,92 @@ package org.firstinspires.ftc.teamcode.Test;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.HardwareSoftware;
-@Disabled
+
+
 @TeleOp(name = "DriveTest")
 public class DriveTest extends OpMode {
 	//Class to test drive subsystem
 	//TEST, NOT MATCHES
-	HardwareSoftware robot = new HardwareSoftware();
+	//HardwareSoftware robot = new HardwareSoftware();
+	public DcMotorEx FRdrive = null;
+	public DcMotorEx BRdrive = null;
+	public DcMotorEx BLdrive = null;
+	public DcMotorEx FLdrive = null;
 
 	public void init() {
-		robot.init(hardwareMap);
+		//init(hardwareMap);
+		FLdrive = hardwareMap.get(DcMotorEx.class, "FLdrive");
+		FRdrive = hardwareMap.get(DcMotorEx.class, "FRdrive");
+		BLdrive = hardwareMap.get(DcMotorEx.class, "BLdrive");
+		BRdrive = hardwareMap.get(DcMotorEx.class, "BRdrive");
 	}
 
 	public void loop() {
 /*		if(gamepad1.x){
-			robot.FLdrive.setVelocity(200, AngleUnit.DEGREES);
+			FLdrive.setVelocity(200, AngleUnit.DEGREES);
 		}
 		if (gamepad1.x) {
-			robot.FRdrive.setVelocity(200, AngleUnit.DEGREES);
+			FRdrive.setVelocity(200, AngleUnit.DEGREES);
 		}
 		if(gamepad1.x){
-			robot.BLdrive.setVelocity(200, AngleUnit.DEGREES);
+			BLdrive.setVelocity(200, AngleUnit.DEGREES);
 		}
 		if(gamepad1.x){
-			robot.BRdrive.setVelocity(200, AngleUnit.DEGREES);
+			BRdrive.setVelocity(200, AngleUnit.DEGREES);
 		}*/
 
-		if(gamepad1.x){
-			robot.FLdrive.setPower(0.9);
+		if (gamepad1.x) {
+			FLdrive.setPower(0.9);
 		}
 		if (gamepad1.x) {
-			robot.FRdrive.setPower(0.9);
+			FRdrive.setPower(0.9);
 		}
-		if(gamepad1.x){
-			robot.BLdrive.setPower(0.9);
+		if (gamepad1.x) {
+			BLdrive.setPower(0.9);
 		}
-		if(gamepad1.x){
-			robot.BRdrive.setPower(0.9);
+		if (gamepad1.x) {
+			BRdrive.setPower(0.9);
 		}
 
-		telemetry.addData("FL", robot.FLdrive.getVelocity(AngleUnit.DEGREES));
-		telemetry.addData("FR", robot.FRdrive.getVelocity(AngleUnit.DEGREES));
-		telemetry.addData("BL", robot.BLdrive.getVelocity(AngleUnit.DEGREES));
-		telemetry.addData("BR", robot.BRdrive.getVelocity(AngleUnit.DEGREES));
+		telemetry.addData("FL", FLdrive.getVelocity(AngleUnit.DEGREES));
+		telemetry.addData("FR", FRdrive.getVelocity(AngleUnit.DEGREES));
+		telemetry.addData("BL", BLdrive.getVelocity(AngleUnit.DEGREES));
+		telemetry.addData("BR", BRdrive.getVelocity(AngleUnit.DEGREES));
 
 
 		if (gamepad1.a) {
-			robot.BLdrive.setPower(0);
-			robot.FLdrive.setPower(0);
-			robot.BRdrive.setPower(0);
-			robot.FRdrive.setPower(0);
+			BLdrive.setPower(0);
+			FLdrive.setPower(0);
+			BRdrive.setPower(0);
+			FRdrive.setPower(0);
 		}
-double pow = 0.4;
+		double pow = 0.4;
 		if (gamepad2.a) {
-			robot.FLdrive.setPower(pow);
+			FLdrive.setPower(pow);
 		} else {
-			robot.FLdrive.setPower(0);
+			FLdrive.setPower(0);
 		}
 
 		if (gamepad2.x) {
-			robot.FRdrive.setPower(pow);
+			FRdrive.setPower(pow);
 		} else {
-			robot.FRdrive.setPower(0);
+			FRdrive.setPower(0);
 		}
 
 		if (gamepad2.b) {
-			robot.BLdrive.setPower(pow);
+			BLdrive.setPower(pow);
 		} else {
-			robot.BLdrive.setPower(0);
+			BLdrive.setPower(0);
 		}
 
 		if (gamepad2.y) {
-			robot.BRdrive.setPower(pow);
+			BRdrive.setPower(pow);
 		} else {
-			robot.BRdrive.setPower(0);
+			BRdrive.setPower(0);
 		}
 	}
 }
