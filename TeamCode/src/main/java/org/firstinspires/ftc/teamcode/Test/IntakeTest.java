18642abf56d6d69b78a9f817b2ec8b71d81280a9
TJPhoenix64 <tyler.jordan@gcsaz.org>
2026-01-30 13:51:35 -0700

    moved from motor to servo for intakes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/IntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/IntakeTest.java
@@ -1,27 +1,30 @@
 package org.firstinspires.ftc.teamcode.Test;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @TeleOp(name = "Intake test")
 public class IntakeTest extends OpMode {
-    DcMotorEx intake = null;
-    @Override
-    public void init() {
-        intake = hardwareMap.get(DcMotorEx.class, "intake");
-    }
+	CRServo intake = null;
 
-    @Override
-    public void loop() {
-        if(gamepad1.a){
-            intake.setPower(0.9);
-        } else if(gamepad1.b){
-            intake.setPower(-0.9);
-        } else {
-            intake.setPower(0);
-        }
-    }
+	@Override
+	public void init() {
+		intake = hardwareMap.get(CRServo.class, "intake");
+	}
+
+	@Override
+	public void loop() {
+		if (gamepad1.a) {
+			intake.setPower(0.5);
+		} else if (gamepad1.b) {
+			intake.setPower(-0.5);
+		} else {
+			intake.setPower(0);
+		}
+	}
 
 
 }
